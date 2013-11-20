a9184a208469aa33b2b6677ceda7116e18eb6827
Michael van Holst <mikevanholst@gmail.com>
2013-11-20 13:50:29 -0500

    adds parent scaffold

---
+++ app/views/parents/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @parent, :parent_name, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BasicNestedForm::Application.routes.draw do
+  resources :parents
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
