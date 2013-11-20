343d5879fde9ce359d4ff4b5d7aadfd90fd64d7a
Michael van Holst <mikevanholst@gmail.com>
2013-11-20 13:49:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicNestedForm::Application.config.secret_key_base = '4398951ae9b59248669ee5419f9b7606403d0b515fea4d2bd2ed7d648be34777e6dfcd1ca67464ef6bcd0423036d8a9c4d5189d494a9fccc060b8f9cb1a1c5bb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BasicNestedForm::Application.config.session_store :cookie_store, key: '_basic_nested_form_session'
