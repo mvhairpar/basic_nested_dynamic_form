4760a88ebb705834ac4d63d00d4ec3402287953b
Michael van Holst <mikevanholst@gmail.com>
2013-11-22 08:10:32 -0500

    brute force appending adds last child

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,12 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+
+function add_fields(link, association, content) {
+ var new_id = new Date().getTime();
+ var regexp = new RegExp("new_" + association, "g")
+ $(link).up().insert({
+ before: content.replace(regexp, new_id)
+ });
+}
--- app/helpers/parents_helper.rb
+++ app/helpers/parents_helper.rb
@@ -1,2 +1,24 @@
 module ParentsHelper
+
+  def link_to_add_fields_template(name, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
+
+  def link_to_add_fields(name_placeholder, f, association)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render("child_fields", :f => builder)
+    end
+    link_to_function('add another child', "add_fields()")
+      # link_to_function("Greeting", "alert('Hello world!')")
+  end
+
+
 end
+
+
+
